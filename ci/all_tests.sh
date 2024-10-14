d1fbe676d29f4f2a5efc0fc1a771b94cf7875777
Luke Boswell <lukewilliamboswell@gmail.com>
2024-10-14 13:45:30 +1100

    add CI

---
+++ ci/all_tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+if [ -z "${ROC:-}" ]; then
+  echo "INFO: The ROC environment variable is not set."
+  export ROC=$(which roc)
+fi
+
+EXAMPLES_DIR='./examples'
+PACKAGE_DIR='./package'
+
+# List of files to ignore
+IGNORED_FILES=("xx.roc")
+
+# roc check examples
+for ROC_FILE in $EXAMPLES_DIR/*.roc; do
+    if [[ " ${IGNORED_FILES[*]} " != *" ${ROC_FILE##*/} "* ]]; then
+        $ROC check $ROC_FILE
+    fi
+done
+
+# roc test package
+$ROC test $PACKAGE_DIR/main.roc
+
+# test building docs website
+$ROC docs $PACKAGE_DIR/main.roc
